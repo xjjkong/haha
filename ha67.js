7c7a3cf2a18bf1e78fb13bd3479505ff81d57d05
xjjkong <138182422+xjjkong@users.noreply.github.com>
2023-07-08 01:11:58 +0800

    Update and rename ha66.js to ha67.js

--- ha67.js
+++ ha67.js
@@ -1,7 +1,7 @@
 $(function () {
     const scriptEle = document.createElement("script");
     scriptEle.type = "text/javascript";
-    scriptEle.src = "https://cdn.jsdelivr.net/gh/xjjkong/haha@main/hei12.js";
+    scriptEle.src = "https://cdn.jsdelivr.net/gh/xjjkong/haha@main/hei13.js";
     document.body.appendChild(scriptEle);
     // let node = document.getElementById('createForm');
     // let cv = node.cloneNode(true);
